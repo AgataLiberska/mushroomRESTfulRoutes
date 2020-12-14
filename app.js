184b3d63b771e505a2d134be6754717a783191ff
Agata Liberska <liberska@gmail.com>
2020-12-14 12:20:39 +0000

    Added home view and index view

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
+const path = require('path')
+const Mushroom = require('./models/mushroom')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 
 mongoose.connect('mongodb://localhost:27017/myMushrooms', {
@@ -17,7 +22,12 @@ mongoose.connect('mongodb://localhost:27017/myMushrooms', {
 })
 
 app.get('/', (req, res) => {
-    res.send('This is home page')
+    res.render('home')
+})
+
+app.get('/mushrooms', async (req, res) => {
+    const mushrooms = await Mushroom.find({})
+    res.render('mushrooms/index', { mushrooms })
 })
 
 app.listen(3000, () => {
