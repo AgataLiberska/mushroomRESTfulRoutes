dbbaefe4a3cf410310b6d241d8400f295890f8f3
Agata Liberska <liberska@gmail.com>
2020-12-14 11:54:18 +0000

    database connection

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose  = require('mongoose')
+
+
+mongoose.connect('mongodb://localhost:27017/myMushrooms', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+.then(() =>{
+    console.log("DB Connection open")
+})
+.catch(err => {
+    console.log("DB Connection error")
+    console.log(err)
+})
 
 app.get('/', (req, res) => {
     res.send('This is home page')
--- models/mushroom.js
+++ models/mushroom.js
@@ -1,4 +1,4 @@
-const mongoose = require 'mongoose'
+const mongoose = require('mongoose')
 
 const mushroomSchema = new mongoose.Schema({
     title: {
