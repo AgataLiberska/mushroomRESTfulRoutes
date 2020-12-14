ecfadde1729e04cd608122852e491b39fddd8366
Agata Liberska <liberska@gmail.com>
2020-12-14 10:40:05 +0000

    Created db model

---
+++ models/mushroom.js
@@ -0,0 +1,18 @@
+const mongoose = require 'mongoose'
+
+const mushroomSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    location: String,
+    dateFound: {
+        type: Date,
+        default: Date.now
+    },
+    edible: Boolean
+})
+
+const Mushroom = mongoose.model('Mushroom', mushroomSchema);
+
+module.exports = Mushroom;
