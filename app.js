dbdf09ee0825c25ffcffd749a70f14544c6a4e2d
Agata Liberska <liberska@gmail.com>
2020-12-14 10:39:30 +0000

    Established server connection

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('This is home page')
+})
+
 app.listen(3000, () => {
     console.log('Connected to server!')
 })
