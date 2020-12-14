278617fdf19d5eba2ba9e57a694603f0fd8e1b9a
Agata Liberska <liberska@gmail.com>
2020-12-14 14:49:50 +0000

    Created a new item route + view

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const Mushroom = require('./models/mushroom')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
+
 
 mongoose.connect('mongodb://localhost:27017/myMushrooms', {
     useNewUrlParser: true,
@@ -30,10 +32,25 @@ app.get('/mushrooms', async (req, res) => {
     res.render('mushrooms/index', { mushrooms })
 })
 
+app.get('/mushrooms/new', (req,res) => {
+    res.render('mushrooms/new')
+})
+
+app.post('/mushrooms', async (req, res) => {
+    const mushroom = new Mushroom(req.body.mushroom)
+    await mushroom.save()
+    res.redirect(`/mushrooms/${mushroom._id}`)
+}) 
+
 app.get('/mushrooms/:id', async (req, res) => {
     const mushroom = await Mushroom.findById(req.params.id)
     res.render('mushrooms/show', { mushroom })
 })
+
+app.get('/mushrooms/:id/edit', (req,res) => {
+    res.render('mushrooms/edit')
+})
+
 
 app.listen(3000, () => {
     console.log('Connected to server!')
